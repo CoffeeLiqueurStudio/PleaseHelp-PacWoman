f8a472ff00cc2a2b13b1d457618be7805603394f
CoffeeLiqueurStudio <coffeeliqueurstudio@gmail.com>
2025-06-04 17:58:25 +0300

    added cycle and car logic

--- minigame/scripts/car/car_script.gd
+++ minigame/scripts/car/car_script.gd
@@ -1,1 +1,21 @@
 extends CharacterBody2D
+@onready var path_follow_2d: PathFollow2D = $Path2D/PathFollow2D
+@export var speed = 500
+@onready var timer: Timer = $Timer
+
+func _physics_process(delta: float) -> void:
+	path_follow_2d.progress += speed * delta
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	match area.name:
+		"Barrier_1":
+			speed = 250
+			#print(speed)
+			timer.start()
+		"Barrier_2":
+			speed = 100
+			#print(speed)
+			timer.start()
+func _on_timer_timeout() -> void:
+	speed = 500
+	#print(speed)
--- minigame/scripts/cycle/cycle_script.gd
+++ minigame/scripts/cycle/cycle_script.gd
@@ -1,1 +1,55 @@
 extends CharacterBody2D
+@onready var state_label: Label = $Label
+@onready var speed_label: Label = $CanvasLayer/Control/Label
+
+var speed: float = 0
+enum States {IDLE, WALK, ACTION}
+var state: States = States.IDLE
+var direction
+var playerdirection = "Down"
+var cycle_hp = 4
+
+func _physics_process(delta: float) -> void:
+	direction = Input.get_vector("left","right","up","down")
+	velocity = direction * speed
+	move_and_slide()
+	boost_speed()
+	print(speed)
+	state_machine()
+	speed_label.text = "Speed" + str(speed)
+
+func state_machine():
+	if velocity != Vector2.ZERO: 
+		state = States.WALK
+	if Input.is_action_just_pressed("ui_accept"):
+		state = States.ACTION
+	if velocity == Vector2.ZERO:
+		state = States.IDLE
+
+	if state == States.IDLE:
+		state_label.text = "State: Idle"
+	elif state == States.WALK:
+		state_label.text = "State: Walk"
+	elif state == States.ACTION:
+		state_label.text = "State: Action"
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	match area.name:
+		"Barrier_1":
+			cycle_hp -= 1
+			print(cycle_hp)
+		"Barrier_2":
+			cycle_hp -= 1
+			print(cycle_hp)
+
+
+func boost_speed():
+	if Input.is_action_just_pressed("speed_boost"):
+		speed += 40
+		if speed > 500:
+			speed = 500
+	for i in 1:
+		speed -= 1
+		if speed < 1:
+			speed = 1
